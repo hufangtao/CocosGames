84765ce62c9a15a70e4030bdf23c38e118015dd9
hufangtao <hufangtao@users.noreply.github.com>
2018-07-22 22:40:53 +0800

    getUserMedia版本更新

--- assets/js/audio/AudioControl.js
+++ assets/js/audio/AudioControl.js
@@ -18,33 +18,44 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
         this._audioContext = new AudioContext();
 
-        if (!navigator.getUserMedia)
-            navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-        if (!navigator.cancelAnimationFrame)
-            navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-        if (!navigator.requestAnimationFrame)
-            navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
-
         var self = this;
-        navigator.getUserMedia(
-            {
-                "audio": {
-                    "mandatory": {
-                        "googEchoCancellation": "false",
-                        "googAutoGainControl": "false",
-                        "googNoiseSuppression": "false",
-                        "googHighpassFilter": "false"
-                    },
-                    "optional": []
-                },
-            }, function (stream) {
+        var constraints = {audio:true,video: true};
+
+        // 老的浏览器可能根本没有实现 mediaDevices，所以我们可以先设置一个空的对象
+        if (navigator.mediaDevices === undefined) {
+            navigator.mediaDevices = {};
+        }
+
+        // 一些浏览器部分支持 mediaDevices。我们不能直接给对象设置 getUserMedia
+        // 因为这样可能会覆盖已有的属性。这里我们只会在没有getUserMedia属性的时候添加它。
+        if (navigator.mediaDevices.getUserMedia === undefined) {
+            navigator.mediaDevices.getUserMedia = function(constraints) {
+
+                // 首先，如果有getUserMedia的话，就获得它
+                var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                // 一些浏览器根本没实现它 - 那么就返回一个error到promise的reject来保持一个统一的接口
+                if (!getUserMedia) {
+                    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+
+                // 否则，为老的navigator.getUserMedia方法包裹一个Promise
+                return new Promise(function(resolve, reject) {
+                    getUserMedia.call(navigator, constraints, resolve, reject);
+                });
+            }
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (mediaStream) {
                 // Create analyser node
                 var audioContext = self._audioContext;
                 var inputPoint = audioContext.createGain();
 
-                var audioInput = audioContext.createMediaStreamSource(stream);
+                var audioInput = audioContext.createMediaStreamSource(mediaStream);
                 audioInput.connect(inputPoint);
 
                 var analyserNode = audioContext.createAnalyser();
@@ -59,11 +70,12 @@ cc.Class({
                 self._audioInput = audioInput;
 
                 self._inited = true;
-            }, function (e) {
+            })
+            .catch(function (err) {
                 alert('Error getting audio');
-                console.log(e);
+                console.log(err.name+":"+err.message);
                 self._inited = false;
-            });
+            })
     },
 
     start () {
